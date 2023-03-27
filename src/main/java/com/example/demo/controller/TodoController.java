41013899cdf062b20e342e85c624b69f548511f6
jihye123 <botopera666@gmail.com>
2023-03-27 16:24:07 +0900

    3/27 수업 완료

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -2,15 +2,16 @@ package com.example.demo.controller;
 
 import com.example.demo.service.TodoService;
 import com.example.demo.dto.ResponseDTO;
+import com.example.demo.model.TodoEntity;
+import com.example.demo.dto.TodoDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("todo")
@@ -24,6 +25,50 @@ public class TodoController {
         List<String> list=new ArrayList<>();
         list.add(str);
         ResponseDTO<String> response=ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
+        try {
+            // test
+            String temporaryUserId="temporary-user";
+
+            // TodoDTO -> TodoEntity
+            TodoEntity entity=TodoDTO.toEntity(dto);
+
+            // entity 생성 시 id는 null값
+            entity.setId(null);
+
+            // 임시 userId
+            entity.setUserId(temporaryUserId);
+
+            // 서비스 레이어 create()-서비스 계층에 저장
+            List<TodoEntity> entities=service.create(entity);
+
+            List<TodoDTO> dtos=entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            ResponseDTO<TodoDTO> response=ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            return ResponseEntity.ok().body(response);
+
+        } catch(Exception e) {
+            String error=e.getMessage();
+            ResponseDTO<TodoDTO> response=ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> retrieveTodoList() {
+        String temporaryUserId="temporary-user";
+
+        List<TodoEntity> entities=service.retrieve(temporaryUserId);
+
+        List<TodoDTO> dtos=entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+        ResponseDTO<TodoDTO> response=ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
         return ResponseEntity.ok().body(response);
     }
 }
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -3,9 +3,13 @@ package com.example.demo.service;
 import com.example.demo.persistence.TodoRepository;
 import com.example.demo.model.TodoEntity;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
 public class TodoService {
 
@@ -17,5 +21,31 @@ public class TodoService {
         repository.save(entity);
         TodoEntity savedEntity=repository.findById(entity.getId()).get();
         return savedEntity.getTitle();
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity) {
+        //Validations
+        validate(entity);
+
+        repository.save(entity);
+
+        log.info("Entity Id: {} is saved.", entity.getId());
+
+        return repository.findByUserId(entity.getUserId());
+    }
+
+    private static void validate(TodoEntity entity) {
+        if(entity ==null) {
+            log.warn("entity cannot be null.");
+            throw new RuntimeException("Entity cannot be null.");
+        }
+        if(entity.getUserId()==null) {
+            log.warn("Unknown user.");
+            throw new RuntimeException("Unknown user.");
+        }
+    }
+
+    public List<TodoEntity> retrieve(final String userId) {
+        return repository.findByUserId(userId);
     }
 }
